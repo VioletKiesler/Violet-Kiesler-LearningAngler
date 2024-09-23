cf0d75462e8baf8ac9df7a185a2973d931517405
VioletKiesler13 <w0845969@myscc.ca>
2024-09-23 14:18:14 -0400

    Finished assigment 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Bird} from "./models/bird";
+import {NgForOf, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgForOf, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'LearningAngler';
+  birdList: Bird [] = [
+    {id: 1, type: "Cockatoo", name: "Jasper", weight: 1.5, isTamed: true },{id: 2, type: "Jardines Parrot", name: "Pickles", weight: 1, isTamed: true},
+    {id: 3, type: "Blue Fronted Amazon", name: "Chico", weight: 3, isTamed: false}, {id: 4, type: "African Grey", name: "Scarlet", weight: 2, isTamed: false}
+  ]
 }
---
+++ src/app/models/bird.ts
@@ -0,0 +1,7 @@
+export interface Bird {
+  id: number,
+  type: string,
+  name: string,
+  weight: number,
+  isTamed?: boolean
+}
